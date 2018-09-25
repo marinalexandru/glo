32610f665422598231163326fb9e37a9705fc330
Alexandru Marin <johndoe@example.commarin.alexandru.andu@gmail.com>
2018-09-25 11:21:04 +0300

    Implementing date pickers

--- UI/Page18Check.xaml.cs
+++ UI/Page18Check.xaml.cs
@@ -8,6 +8,8 @@ using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Automation.Provider;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
@@ -20,12 +22,27 @@ using Windows.UI.Xaml.Navigation;
 
 namespace kent_glo_20180830.UI
 {
+
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class Page18Check : Page
     {
+
+
+        public DateTimeOffset ChosenDay { get; set; }
+        public DateTimeOffset ChosenMonth { get; set; }
+        public DateTimeOffset ChosenYear { get; set; }
+
         private MediaPlayerPage mediaPlayerPage;
+
+        private enum CALENDAR_DISPLAY
+        {
+            DAY_ONLY,
+            MONTH_ONLY,
+            YEAR_ONLY
+        }
 
         public Page18Check()
         {
@@ -35,17 +52,34 @@ namespace kent_glo_20180830.UI
 
         private void Page18Check_Loaded(object sender, RoutedEventArgs e)
         {
+            configureFlyouts();
             mediaPlayerPage = Utils.Utils.FindParent<MediaPlayerPage>(this);
             Storyboard storyboard = this.Resources["ImageFadeIn"] as Storyboard;
             storyboard.Begin();
-            storyboard.Completed += (_1,_2) => showYesNoButtons();
-            
+            storyboard.Completed += (_1, _2) => showYesNoButtons();
+        }
+
+        private void configureFlyouts()
+        {
+            setDatePicker(CALENDAR_DISPLAY.DAY_ONLY, (day.Flyout as DatePickerFlyout));
+            setDatePicker(CALENDAR_DISPLAY.MONTH_ONLY, (month.Flyout as DatePickerFlyout));
+            setDatePicker(CALENDAR_DISPLAY.YEAR_ONLY, (year.Flyout as DatePickerFlyout));
+
+        }
+
+        private void setDatePicker(CALENDAR_DISPLAY calendarDisplay, DatePickerFlyout datePickerFlyout)
+        {
+            datePickerFlyout.DayVisible = calendarDisplay == CALENDAR_DISPLAY.DAY_ONLY;
+            datePickerFlyout.MonthVisible = calendarDisplay == CALENDAR_DISPLAY.MONTH_ONLY;
+            datePickerFlyout.YearVisible = calendarDisplay == CALENDAR_DISPLAY.YEAR_ONLY;
         }
 
         private void showYesNoButtons()
         {
             yes.Visibility = Visibility.Visible;
-            no.Visibility = Visibility.Visible;
+            day.Visibility = Visibility.Visible;
+            month.Visibility = Visibility.Visible;
+            year.Visibility = Visibility.Visible;
         }
 
         private void yes_Tapped(object sender, TappedRoutedEventArgs e)
@@ -54,7 +88,8 @@ namespace kent_glo_20180830.UI
             storyboard.Completed += (_1, _2) => navigateForward();
             mediaPlayerPage.loadVideo(PageA.A, MediaPlayerPage.VIDEO_STATE.NO_LOOP);
 
-            Task.Delay(200).ContinueWith(async t => {
+            Task.Delay(200).ContinueWith(async t =>
+            {
                 await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
                     storyboard.Begin();
@@ -78,6 +113,23 @@ namespace kent_glo_20180830.UI
         private void navigateBack()
         {
             mediaPlayerPage.navigateTo(typeof(PageNoCustomerLoop));
+        }
+
+        private void day_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //var ap = new ButtonAutomationPeer(Picker);
+            //var ip = ap.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+            //ip?.Invoke();
+        }
+
+        private void month_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void year_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
         }
     }
 }
